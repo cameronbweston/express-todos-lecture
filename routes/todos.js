7f12ee2b18530ef184ce773231455b1d41459be6
Cameron Weston <cameronbweston@gmail.com>
2021-07-13 10:17:45 -0500

    Add render todos to todo.js

--- routes/todos.js
+++ routes/todos.js
@@ -1,9 +1,15 @@
 import { Router } from 'express'
+import * as todoDb from '../data/todo-db.js'
 const router = Router()
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource')
+router.get('/', (req, res) => {
+  todoDb.find({}, function(error, todos) {
+    res.render('todos/index', {
+      todos,
+      error
+    })
+  })
 })
 
 export {
